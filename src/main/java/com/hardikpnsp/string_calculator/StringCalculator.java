a9396b36184fde55fe597b3a87fd701da0cf4b28
Hardik Prajapati <hardikpnsp@gmail.com>
2020-05-24 21:29:28 +0530

    ENH: handle delimiter definition in string

--- src/main/java/com/hardikpnsp/string_calculator/StringCalculator.java
+++ src/main/java/com/hardikpnsp/string_calculator/StringCalculator.java
@@ -5,7 +5,14 @@ public class StringCalculator {
         if(numbers.isEmpty()){
             return 0;
         }else{
-            String[] numTokens = numbers.split(",|\n");
+            String delimiterPattern = ",|\n";
+            if(numbers.startsWith("//")){
+                String[] temp = numbers.split("\n", 2);
+                String extra_delimiter = temp[0].substring(2);
+                numbers = temp[1];
+                delimiterPattern += "|" + extra_delimiter;
+            }
+            String[] numTokens = numbers.split(delimiterPattern);
             if(numTokens.length == 1){
                 return Integer.parseInt(numbers);
             }else{
