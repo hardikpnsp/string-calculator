4ca03acee6a09fc2e1e419d10f7f56bbccb2935d
Hardik Prajapati <hardikpnsp@gmail.com>
2020-05-24 19:03:16 +0530

    CLN: test class parameterized

--- src/test/java/com/hardikpnsp/string_calculator/StringCalculatorTest.java
+++ src/test/java/com/hardikpnsp/string_calculator/StringCalculatorTest.java
@@ -2,13 +2,37 @@ package com.hardikpnsp.string_calculator;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import java.util.List;
+import java.util.Arrays;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
 public class StringCalculatorTest {
+
+    private String inputString;
+    private int expectedOutput;
+    private String testCaseExplaination;
+
+    public StringCalculatorTest(String inputString, int expectedOutput, String testCaseExplaination){
+        this.inputString = inputString;
+        this.expectedOutput = expectedOutput;
+        this.testCaseExplaination = testCaseExplaination;
+    }
+
+    @Parameterized.Parameters
+    public static List<Object[]> testCases(){
+        return Arrays.asList(new Object[][]{
+            {"", 0, "Empty string should return 0"},
+            {"42", 42, "Single digit string should return the digit itself"},
+            {"1,1", 2, "String with ',' as delimiter should return sum of the digits seperated by ','"},
+        });
+    }
+
     @Test
     public void testAdd(){
-        assertEquals(0, StringCalculator.add(""));
-        assertEquals(42, StringCalculator.add("42"));
-        assertEquals(2, StringCalculator.add("1,1"));
+        assertEquals(testCaseExplaination, expectedOutput, StringCalculator.add(inputString));
     }
 }
