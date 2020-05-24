de35b9481a529d968ea387a424925009b9dd629d
Hardik Prajapati <hardikpnsp@gmail.com>
2020-05-24 17:53:35 +0530

    TST: added test for add method

--- src/test/java/com/hardikpnsp/string_calculator/StringCalculatorTest.java
+++ src/test/java/com/hardikpnsp/string_calculator/StringCalculatorTest.java
@@ -1,5 +1,12 @@
 package com.hardikpnsp.string_calculator;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class StringCalculatorTest {
-    
+    @Test
+    public void testAdd(){
+        assertEquals(0, StringCalculator.add(""));
+    }
 }
