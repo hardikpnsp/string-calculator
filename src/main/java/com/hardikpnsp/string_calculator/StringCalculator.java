353e1726b9dcf0e2920db56d1b078d0593b5eee1
Hardik Prajapati <hardikpnsp@gmail.com>
2020-05-25 08:39:39 +0530

    CLN: removed redundancy and variable declarations moved to top

--- src/main/java/com/hardikpnsp/string_calculator/StringCalculator.java
+++ src/main/java/com/hardikpnsp/string_calculator/StringCalculator.java
@@ -8,36 +8,31 @@ public class StringCalculator {
             return 0;
         }else{
             String delimiterPattern = ",|\n";
+            String exceptionMessage = "Negative numbers encountered in input: ";
+            Vector<Integer> negativeNumbers = new Vector<Integer>();
+            int answer = 0;
+
             if(numbers.startsWith("//")){
                 String[] temp = numbers.split("\n", 2);
                 String extra_delimiter = temp[0].substring(2);
                 numbers = temp[1];
                 delimiterPattern += "|" + extra_delimiter;
             }
+
             String[] numTokens = numbers.split(delimiterPattern);
-            if(numTokens.length == 1){
-                int n = Integer.parseInt(numbers);
+            for (String num : numTokens){
+                int n = Integer.parseInt(num);
                 if(n < 0){
-                    throw new NegativeNotAllowedException("Negative numbers encountered in input: " + "[" + numbers + "]");
+                    negativeNumbers.add(n);
                 }else{
-                    return n;
+                    answer += n;
                 }
+            }
+
+            if(negativeNumbers.isEmpty()){
+                return answer;
             }else{
-                int answer = 0;
-                Vector<Integer> negativeNumbers = new Vector<Integer>();
-                for (String num : numTokens){
-                    int n = Integer.parseInt(num);
-                    if(n < 0){
-                        negativeNumbers.add(n);
-                    }else{
-                        answer += n;
-                    }
-                }
-                if(negativeNumbers.isEmpty()){
-                    return answer;
-                }else{
-                    throw new NegativeNotAllowedException("Negative numbers encountered in input: " + negativeNumbers.toString());
-                }
+                throw new NegativeNotAllowedException(exceptionMessage + negativeNumbers.toString());
             }
         }
     }
