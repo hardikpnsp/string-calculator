aade24965dbf113197ae452e4e3f1584485b8d47
Hardik Prajapati <hardikpnsp@gmail.com>
2020-05-25 10:07:18 +0530

    CLN: no need to serialize thus warning supressed

--- src/main/java/com/hardikpnsp/string_calculator/NegativeNotAllowedException.java
+++ src/main/java/com/hardikpnsp/string_calculator/NegativeNotAllowedException.java
@@ -1,5 +1,6 @@
 package com.hardikpnsp.string_calculator;
 
+@SuppressWarnings("serial")
 public class NegativeNotAllowedException extends Exception {
 	NegativeNotAllowedException(String errorMessage){
         super(errorMessage);
