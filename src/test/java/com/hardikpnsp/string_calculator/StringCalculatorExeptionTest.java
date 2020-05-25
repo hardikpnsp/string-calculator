a391d330c60d76fd057336caa3cc996170a9e981
Hardik Prajapati <hardikpnsp@gmail.com>
2020-05-25 07:56:30 +0530

    CLN: made Exception Testing class parameterized

--- src/test/java/com/hardikpnsp/string_calculator/StringCalculatorExeptionTest.java
+++ src/test/java/com/hardikpnsp/string_calculator/StringCalculatorExeptionTest.java
@@ -1,11 +1,29 @@
 package com.hardikpnsp.string_calculator;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
 public class StringCalculatorExeptionTest {
     
+    private String inputString;
+
+    public StringCalculatorExeptionTest(String inputString){
+        this.inputString = inputString;
+    }
+
+    @Parameterized.Parameters
+    public static List<Object[]> testCases(){
+        return Arrays.asList(new Object[][]{
+            {"-1"},
+        });
+    }
     @Test(expected = NegativeNotAllowedException.class)
     public void testAddNegativeNumbers() throws NegativeNotAllowedException{
-        StringCalculator.add("-1");
+        StringCalculator.add(inputString);
     }
 }
