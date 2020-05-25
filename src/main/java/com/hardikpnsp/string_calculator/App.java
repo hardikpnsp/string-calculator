df4a38225667e6ba0b7d9b71aaf8565657128e96
Hardik Prajapati <hardikpnsp@gmail.com>
2020-05-25 10:02:44 +0530

    ENH: added a simple CLI for interaction

--- src/main/java/com/hardikpnsp/string_calculator/App.java
+++ src/main/java/com/hardikpnsp/string_calculator/App.java
@@ -1,5 +1,7 @@
 package com.hardikpnsp.string_calculator;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Enter String Sequence to calculate Addition: " );
+        Scanner in = new Scanner(System.in);
+        String inputString = in.nextLine();
+        in.close();
+    
+        inputString = inputString.replace("\\n", "\n");
+
+        int answer;
+		try {
+			answer = StringCalculator.add(inputString);
+            System.out.println("Answer: " + answer);
+		} catch (NegativeNotAllowedException e) {
+            System.out.println(e.getMessage());
+			System.out.println("Please do not enter negative numbers");
+		}
     }
 }
